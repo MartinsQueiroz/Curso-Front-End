e06352a390f3b8d2df5ee602ffa16566f0cee392
Lucas <104466995+MartinsQueiroz@users.noreply.github.com>
2022-09-29 11:14:30 -0300

    Delete Trilha-fluig directory

--- Trilha-fluig/Js/objeto.js
+++
@@ -1,9 +0,0 @@
-function externo(){
-    var pessoa = {
-        nome         : "Lucas",
-        sobrenome    : "Martins",
-        departamento : "fluig"
-    };
-
-    document.getElementById("local").innerHTML = pessoa.nome + " trabalha no " + pessoa.departamento + "<br />" + pessoa.nome + " " + pessoa.sobrenome;
-}
