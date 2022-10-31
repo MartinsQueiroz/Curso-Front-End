aaccdcfc9f359bfc052df7ef61fe44f8daea1111
lucas <lucasqueiroz@outlook.com>
2022-10-31 17:20:20 -0300

    Atualizando repositorios

--- JavaScript/javascript/exercicio.js
+++ JavaScript/javascript/exercicio.js
@@ -2,10 +2,10 @@ function obterMedia(){
 
     var a = parseInt(document.getElementById("a").value);
     var b = parseInt(document.getElementById("b").value);
-    var c = parseInt(document.getElementById("c").value);
+    
 
-    var media = (a + b + c) /3; 
+    var soma = (a + b); 
 
-    document.getElementById("resultado").innerHTML = "Resultado:" + media;
+    document.getElementById("resultado").innerHTML = "Resultado:" + soma;
 
 }
