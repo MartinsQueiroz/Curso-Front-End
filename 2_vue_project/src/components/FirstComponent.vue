68021034d959879acbd1cfec33cfb9ed34d57f2c
Lucas Martins <lucasmqueiroz@outlook.com>
2023-07-17 22:30:16 -0300

    Estudo de caso com Vue

---
+++ 2_vue_project/src/components/FirstComponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1>Olá Vue!</h1>
+        <p>Meu nome é {{ nome }} e trabalho como {{ profissao }}</p>
+    </div>
+    
+</template>
+
+<script>
+    export default{
+        name:'FirstComponent',
+        data(){
+            return {
+                nome: "Lucas",
+                profissao: "Programador"
+            }
+        }
+    }
+</script>
---
+++ 2_vue_project/src/components/Info.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Estou estudando programação no momento</p>
+        <p>Utilizo as seguintes tecnologias: </p>
+        <ul>
+            <li>Javascript</li>
+            <li>ReactJs</li>
+            <li>Vue3</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Info'
+    }
+
+</script>
