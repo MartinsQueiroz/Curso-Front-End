5a08afe3200ce68418c477c3b40fe68958e5f448
Lucas Martins <lucasmqueiroz@outlook.com>
2023-08-11 16:45:49 -0300

    code together

---
+++ react-components/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './components/Header'
+import CalculatorIMC from './components/IMC'
+
+function App() {
+  return (
+    <div className="App">
+      <Header title="Titulo do APP"/>
+      <CalculatorIMC />
+    </div>
+  );
+}
+
+export default App;
